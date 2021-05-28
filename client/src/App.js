88a6d37bf43d19cafc25b269db466197ccfdbafe
Tsotetzo Willy <w.tsotetzo@b-a-w.com>
2021-05-28 14:21:52 +0200

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">Homement</Link>
+          <Link to="/otherpage">Other Page seulement</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
